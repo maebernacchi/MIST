a033e2230fd5b8e3dcf96b897f7c9e4167363977
Sumin Goh <gohsumin@grinnell.edu>
2020-07-28 11:10:11 -0400

    fixed dimensions of the function bar; set up more global variables

--- frontend/src/workspace/PopupCanvas.js
+++ frontend/src/workspace/PopupCanvas.js
@@ -1,203 +1,240 @@
 import React, { useState } from "react";
 import Portal from "./Portal";
 import { Rect, Group, Text } from "react-konva";
+import { Link } from "react-router-dom";
 import gui from "./mistgui-globals.js";
+import { width, height } from "./globals";
+import popupDimensions from "./globals-popup_canvas-dimensions";
 import MISTImage from "./MISTImageCreate";
 import "../styleSheets/FunBar.css";
 
-
 function PopupCanvas(props) {
+  const [imageName, setImageName] = useState("");
 
-    const [imageName, setImageName] = useState("");
-
-    return (
-        <Portal>
-            <Background {...props} />
-            <PortalImage {...props} />
-            <PortalFunction {...props} />
-            <PortalTextBox {...props} setImageName={setImageName} />
-            <PortalButtons {...props} imageName={imageName} />
-        </Portal>
-    );
+  return (
+    <>
+      <Background {...props} />
+      <Portal>
+        <PortalTextBox {...props} setImageName={setImageName} />
+        <PortalImage {...props} />
+        <PortalFunction {...props} />
+      </Portal>
+      <PortalButtons {...props} imageName={imageName} />
+    </>
+  );
 }
 
 function Background(props) {
-    return (
-        <Group>
-            <div // div makes workspace background dark
-                style={{
-                    position: "absolute",
-                    top: props.top,
-                    left: props.left,
-                    width: gui.width,
-                    height: gui.height,
-                    backgroundColor: "black",
-                    opacity: 0.7,
-                }}
-            />
-            <div // white rounded square background
-                style={{
-                    position: "absolute",
-                    top: props.top + 0.5 * gui.popSaveGroupY,
-                    left: props.left + gui.popSaveGroupX,
-                    width: gui.popRectWidth,
-                    height: gui.popRectHeight,
-                    borderRadius: 25,
-                    backgroundColor: gui.popRectColor,
-                }}
-            />
-        </Group>
-    );
+  return (
+    <Group x={props.x} y={props.y}>
+      <Rect
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        fill={"black"}
+        opacity={0.7}
+      />
+      <Rect
+        x={popupDimensions.canvasX}
+        y={popupDimensions.canvasY}
+        width={popupDimensions.canvasWidth}
+        height={popupDimensions.canvasHeight}
+        fill={"white"}
+        cornerRadius={30}
+        opacity={0.8}
+      />
+    </Group>
+  );
+}
+
+function PortalTextBox(props) {
+  return (
+    <div // Text box: "Enter Name of Image"
+      style={{
+        position: "absolute",
+        top: props.top + popupDimensions.textfieldY,
+        left: props.left + popupDimensions.textfieldX,
+        fontSize: gui.popTextFontSize,
+        fontFamily: gui.functionFont,
+        width: popupDimensions.textfieldWidth,
+        height: popupDimensions.textfieldHeight,
+        textAlign: "center",
+        alignItems: "center",
+      }}
+    >
+      <input
+        type={"text"}
+        placeholder={"Enter Name Of Image"}
+        style={{
+            width: popupDimensions.textfieldWidth,
+            height: popupDimensions.textfieldHeight,
+          border: "2px solid #008CBA",
+          textAlign: "center",
+        }}
+        onChange={(e) => props.setImageName(e.target.value)}
+      />
+    </div>
+  );
 }
 
 function PortalImage(props) {
-
-    return (
-        <MISTImage
-            x={props.left + gui.popCanvasShiftX}
-            y={props.top + gui.popCanvasShiftY}
-            width={gui.popCanvasSide}
-            height={gui.popCanvasSide}
-            renderFunction={props.renderFunction.renderFunction}
-            automated={true}
-        />
-    );
+  return (
+    <MISTImage
+      x={props.left + popupDimensions.imageX}
+      y={props.top + popupDimensions.imageY}
+      width={popupDimensions.imageWidth}
+      height={popupDimensions.imageHeight}
+      //renderFunction={"x"}
+      renderFunction={props.renderFunction.renderFunction}
+      automated={true}
+    />
+  );
 }
 
 function PortalFunction(props) {
-    return (
-        <div // Displays render function
-            style={{
-                position: "absolute",
-                top: props.top + gui.popTextShiftY,
-                left: props.left + gui.popSaveGroupX + gui.popTextShiftX,
-                fontSize: gui.popTextFontSize,
-                fontFamily: gui.functionFont,
-                textAlign: "center",
-                verticalAlign: "middle",
-                width: gui.popTextWidth,
-                height: gui.popTextHeight,
-                overflow: "auto",
-            }}
-        >
-            <p>{props.renderFunction.renderFunction}</p>
-        </div>
-    );
-}
-
-
-function PortalTextBox(props) {
-    return (
-        <div // Text box: "Enter Name of Image"
-            style={{
-                position: "absolute",
-                top: props.top + 0.8 * gui.popSaveGroupY,
-                left: props.left + gui.popTextShiftX + gui.popSaveGroupX,
-                fontSize: gui.popTextFontSize,
-                fontFamily: gui.functionFont,
-                width: gui.popTextWidth,
-                height: gui.popTextHeight,
-                textAlign: "center",
-            }}
-        >
-            <form>
-                <input
-                    type={"text"}
-                    placeholder={"Enter Name Of Image"}
-                    style={{
-                        width: 0.7 * gui.popTextWidth,
-                        height: 0.7 * gui.popTextHeight,
-                        border: "2px solid #008CBA",
-                    }}
-                    onChange={e => props.setImageName(e.target.value)}
-                />
-            </form>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: props.top + popupDimensions.rfTextY,
+        left: props.left + popupDimensions.rfTextX,
+        fontSize: 20,
+        textAlign: "center",
+        width: popupDimensions.rfTextWidth,
+        height: popupDimensions.rfTextHeight,
+        overflow: "auto",
+      }}
+    >
+      <p style={{
+          width: popupDimensions.rfTextWidth,
+          height: popupDimensions.rfTextHeight,
+          fill: 'red'
+      }}>{props.renderFunction.renderFunction}</p>
+    </div>
+  );
 }
 
 function PortalButtons(props) {
-    return (
-        <div
-            style={{
-                position: "absolute",
-                top: props.top + gui.popSaveGroupY + gui.popCanvasSide + gui.popTextHeight * 2,
-                left: props.left + gui.popTextShiftX + gui.popSaveGroupX,
-                fontSize: gui.popTextFontSize,
-                fontFamily: gui.functionFont,
-                width: gui.popTextWidth,
-                height: gui.popTextHeight,
-                textAlign: "center",
+  return (
+    <Group
+      x={props.x + popupDimensions.buttonX}
+      y={props.y + popupDimensions.buttonY}
+    >
+      {["Cancel", "Download"].map((u, i) => {
+        return (
+          <Group
+            x={popupDimensions.buttonMargin + i * popupDimensions.buttonOffset}
+            onClick={() => {
+              if (u === "Cancel") {
+                props.closePortal();
+              }
             }}
-        >
-            {["Cancel", "Download"].map((u, i) => {
-                return (
-                    <button
-                        class="button button2"
-                        onClick={() => {
-                            if (u === "Cancel") {
-                                props.closePortal();
-                            }
-                        }}
-                    >
-                        {u}
-                    </button>
-                );
-            })}
-            <SaveButton {...props} />
-        </div>
-    );
+          >
+            <Rect
+              fill={"white"}
+              stroke={"#008CBA"}
+              strokeWidth={2}
+              width={popupDimensions.buttonWidth}
+              height={popupDimensions.buttonHeight}
+            />
+            <Text
+              fontSize={15}
+              text={u}
+              width={popupDimensions.buttonWidth}
+              height={popupDimensions.buttonHeight}
+              align={"center"}
+              verticalAlign={"middle"}
+            />
+          </Group>
+        );
+      })}
+      <SaveButton {...props} index={2} />
+      <ExpertButton {...props} index={3} />
+    </Group>
+  );
 }
 
 function SaveButton(props) {
+  const [imageExists, setImageExists] = useState("initial");
 
-    const [imageExists, setImageExists] = useState("initial");
+  function SaveImage() {
+    //var newName = props.imageName;
+    //newName = removeOuterWhiteSpace(newName);
+    //var response = getImageExists(newName);
+    console.log("response = " + imageExists);
+  }
 
-    function SaveImage() {
-        //var newName = props.imageName;
-        //newName = removeOuterWhiteSpace(newName);
-        //var response = getImageExists(newName);
-        console.log("response = " + imageExists);
-    }
+  function getImageExists(title) {
+    let url = "api/?action=imageexists&title=" + title;
+    fetch(url)
+      .then((req) => req.json())
+      .then((exists) => setImageExists(exists));
+  }
 
-    function getImageExists(title) {
-        let url = 'api/?action=imageexists&title=' + title;
-        fetch(url)
-          .then(req => req.json())
-          .then(exists => setImageExists(exists));
-    }
+  return (
+    <Group
+      x={
+        popupDimensions.buttonMargin +
+        props.index * popupDimensions.buttonOffset
+      }
+      onClick={() => SaveImage()}
+    >
+      <Rect
+        fill={"white"}
+        stroke={"#008CBA"}
+        strokeWidth={2}
+        width={popupDimensions.buttonWidth}
+        height={popupDimensions.buttonHeight}
+      />
+      <Text
+        fontSize={15}
+        text={"Save"}
+        width={popupDimensions.buttonWidth}
+        height={popupDimensions.buttonHeight}
+        align={"center"}
+        verticalAlign={"middle"}
+      />
+    </Group>
+  );
+}
 
-    return (
-        <Group
-            class="button button2"
-            onClick={() => SaveImage()}
-        >
-            <Rect
-                x={0}
-                y={0}
-                fill={'#A0A3A3'}
-                stroke={'black'}
-                strokeWidth={1}
-                shadowColor={'black'}
-                shadowEnabled={false}
-            >
-                <Text x={0} fill={'black'} fontSize={16}>
-                    Save
-            </Text>
-            </Rect>
-        </Group>
-    );
+function ExpertButton(props) {
+  return (
+    <Group
+      x={
+        popupDimensions.buttonMargin +
+        props.index * popupDimensions.buttonOffset
+      }
+    >
+      <Rect
+        fill={"white"}
+        stroke={"#008CBA"}
+        strokeWidth={2}
+        width={popupDimensions.buttonWidth}
+        height={popupDimensions.buttonHeight}
+      />
+      <Text
+        fontSize={15}
+        text={"Expert"}
+        width={popupDimensions.buttonWidth}
+        height={popupDimensions.buttonHeight}
+        align={"center"}
+        verticalAlign={"middle"}
+      />
+    </Group>
+  );
 }
 
 /**
-* removeOuterWhiteSpace takes a string and removes white space at the beginning and end
-* of the string, but not the white space in the middle of the string.
-* returns a string
-*/
+ * removeOuterWhiteSpace takes a string and removes white space at the beginning and end
+ * of the string, but not the white space in the middle of the string.
+ * returns a string
+ */
 var removeOuterWhiteSpace = function (string) {
-    string = string.replace(/^ */, "");
-    string = string.replace(/ *$/, "");
-    return string;
-}; 
+  string = string.replace(/^ */, "");
+  string = string.replace(/ *$/, "");
+  return string;
+};
 
-export default PopupCanvas  
+export default PopupCanvas;
