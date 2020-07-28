4c4571fafe124301ee62ed2ec0c87ba5e7c34df2
Sumin Goh <gohsumin@grinnell.edu>
2020-07-28 15:55:20 -0400

    dimensions

--- frontend/src/workspace/globals-funbar-dimensions.js
+++ frontend/src/workspace/globals-funbar-dimensions.js
@@ -2,14 +2,26 @@ import {width, height, funBarHeight} from './globals';
 
 const funbarY = height - funBarHeight;
 
-const margin = funBarHeight / 7;
+const margin = funBarHeight * 0.2;
 
-const functionButtonX = width * 34 / 40;
-const imageButtonX = width * 37 / 40;
+const rfTextAreaHeight = funBarHeight - 2 * margin;
+
+const functionButtonX = width * 0.85;
+const functionButtonWidth = width * 0.06;
+const functionButtonHeight = funBarHeight - 2 * margin;
+
+const imageButtonX = width * 0.92;
+const imageButtonWidth = width * 0.06;
+const imageButtonHeight = funBarHeight - 2 * margin;
 
 export default {
     funbarY,
     margin,
+    rfTextAreaHeight,
     functionButtonX,
-    imageButtonX
+    functionButtonWidth,
+    functionButtonHeight,
+    imageButtonX,
+    imageButtonWidth,
+    imageButtonHeight
 }
