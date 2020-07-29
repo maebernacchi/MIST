2bea0f5a540822356360282349b35f785b6ec2f6
Davin Lin <lindavin@grinnell.edu>
2020-07-29 16:26:25 -0400

    Documentation

--- frontend/src/expert/components/Popup.js
+++ frontend/src/expert/components/Popup.js
@@ -1,10 +1,29 @@
-import PropTypes from 'prop-types';
+// +-------+------------------------------------------------------------------------
+// | Notes |
+// +-------+
+/*
+ * Popup.js
+ * This is our Popup Component that is used by the Expert component to create
+ * popups. Or rather, we do not create popups, but rerender this component whenever
+ * we need one in a situation where we need to confirmation for an action.
+ * 
+ */
+
+ import PropTypes from 'prop-types';
 import React from 'react';
 import {
     Button,
     Modal,
 } from 'react-bootstrap';
 
+/*
+ * Our Popup React component. For props we expect to receive:
+ * isOpen, 
+ * onClose,
+ * message,
+ * onConfirm.
+ * which we elaborate in the propTypes immediately after. 
+ */
 function Popup(props) {
     return (
         <Modal show={props.isOpen} onHide={props.onClose}>
@@ -25,6 +44,13 @@ function Popup(props) {
         </Modal>)
 }
 
+/*
+ * isOpen: a boolean that represents whether or not Popup is shown
+ * message: a string that represents the contents of the popup
+ * onConfirm: a function that is the action we need a confirmation for
+ * onClose: a function that is what needs to happen when we close our modal, additionally
+ *  we need to set isOpen to false within this function
+ */
 Popup.propTypes = {
     isOpen: PropTypes.bool,
     message: PropTypes.string,
