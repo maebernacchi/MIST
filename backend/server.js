fe68c2cb508b93b6e42ec4aaf27690cbb4975acd
Himanshu Bainwala <43100411+bainwala@users.noreply.github.com>
2020-08-07 14:37:51 -0700

    added environment secret

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,12 @@ app.use(flash());
 
 app.use(
   session({
-    secret: "some",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
   })
 );
-app.use(cookieParser("some"));
+app.use(cookieParser(process.env.SECRET));
 
 
 //=================== Setup Passport =======================
