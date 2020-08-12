453f0a66362f41da8bf3e822e891f373e3245327
Davin Lin <lindavin@grinnell.edu>
2020-08-12 12:30:47 -0400

    Add database functions for hiding content

--- backend/app/database.js
+++ backend/app/database.js
@@ -1162,6 +1162,109 @@ module.exports.deleteAlbum = async (albumId) => {
   )
 }
 
+// +----------+----------------------------------------------------------
+// | Reporting/Hiding/Blocking |
+// +---------------------------+
+
+// not tested - need front-end for that
+/**
+ * hide content from a user
+ * @param userid: the objectId of the user wanting to hide something
+ * @param type: the type of content being hidden: "comment", "image", or "album" 
+ * @param contentid: the objectId of the content being hidden 
+ * return true if successfull, false elsewise 
+ */
+module.exports.hideContent = async (userid, type, contentid) => {
+  userid = sanitize(userid);
+  type = sanitize(type);
+  contentid = sanitize(contentid);
+  try {
+    let update;
+    switch (type) {
+      case "comment":
+        update = { $push: { "hidden.commentIds": contentid } }
+        break;
+      case "album":
+        update = { $push: { "hidden.albumIds": contentid } }
+        break;
+      case "image":
+        update = { $push: { "hidden.imageIds": contentid } }
+        break;
+      default:
+        throw "invalid type";
+    }
+    const { nModified } = await User.updateOne({ _id: userid }, update).exec();
+    return (Boolean)(nModified);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// not tested - need front-end for that
+/**
+ * unhide content from a user
+ * @param userid: the objectId of the user wanting to unhide something
+ * @param type: the type of content being hidden: "comment", "image", or "album" 
+ * @param contentid: the objectId of the content being unhidden 
+ * return true if successfull, false elsewise 
+ */
+module.exports.unhideContent = async (userid, type, contentid) => {
+  try {
+    let update;
+    switch (type) {
+      case "comment":
+        update = { $pull: { "hidden.commentIds": contentid } }
+        break;
+      case "album":
+        update = { $pull: { "hidden.albumIds": contentid } }
+        break;
+      case "image":
+        update = { $pull: { "hidden.imageIds": contentid } }
+        break;
+      default:
+        throw "invalid type";
+    }
+    const { nModified } = await User.updateOne({ _id: userid }, update).exec();
+    return (Boolean)(nModified);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// not tested - need front-end for that
+/**
+ * blocks a user
+ * @param userid: the objectId of the user who wants to block a user 
+ * @param contentid: the objectId of the user to be blocked 
+ * returns true if successfull, false otherwise
+ */
+module.exports.blockUser = async (userid, contentid) => {
+  try {
+    const { nModified } = await User.updateOne({ _id: userid }, { $push: { blockedUsers: contentid } }).exec();
+    return (Boolean)(nModified);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// not tested - need front-end for that
+/**
+ * unblocks a user
+ * @param userid: the objectId of the user who wants to block a user 
+ * @param contentid: the objectId of the user to be blocked 
+ * returns true if successfull, false otherwise
+ */
+module.exports.unblockUser = async (userid, contentid) => {
+  try {
+    const { nModified } = await User.updateOne({ _id: userid }, { $pull: { blockedUsers: contentid } }).exec();
+    return (Boolean)(nModified);
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
 // +-------+-------------------------------------------------
 // | Misc. |
 // +-------+
