b0b71846fd479e64afa4f2b50f0c243a22607801
zaen323 <chouzaen2002@gmail.com>
2021-07-01 11:05:39 -0500

    created CRUD routes in routes.js

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -1,165 +1,231 @@
+const pool = require("../db/dbconfig");
 const api = require("./api");
 const database = require("./database");
 
 module.exports = (app) => {
-  // Path: /api
-  //  Dynamic content distribution - return raw data through Fetch
+	// Our stuff
+	// ROUTES
+	// create
+	app.post("/users", async (req, res) => {
+		try {
+			const { user_id, email, password } = req.body;
+			const newUser = await pool.query(
+				"insert into users(user_id, email, password) values ($1, $2, $3)",
+				[user_id, email, password]
+			);
+			res.json(newUser);
+		} catch (error) {
+			console.error(error.message);
+		}
+	});
 
-  app.post("/api", function (req, res) {
-    api.run(req.body, req, res);
-  });
-  app.get("/api", function (req, res) {
-    api.run(req.query, req, res);
-  });
-  //------------------------------------------------
+	// read
+	app.get("/users", async (req, res) => {
+		try {
+			const allUsers = await pool.query("select * from users");
+			res.json(allUsers.rows);
+		} catch (error) {
+			console.error(error.message);
+		}
+	});
 
-  app.get("/api/profiles/:username", (req, res) => {
-    const username = req.params.username;
-    const user = database.User.findOne({ username: username });
-    //const user = null;
-    if (user === null) {
-      user = {
-        forename: "shrek",
-        surname: "the ogre",
-        username: "theShrekster",
-        createdAt: new Date().toDateString(),
-        about: "first the worst, shrek the best",
-        profilepic: "sin(x)",
-      };
-    }
+	app.get("/users/:user_id", async (req, res) => {
+		try {
+			const { user_id } = req.params; // {user_id: "evelyn"}
+			const user = await pool.query("select * from users where user_id = $1", [
+				user_id,
+			]);
+			res.json(user.rows[0]);
+		} catch (error) {
+			console.error(error.message);
+		}
+	});
 
-    const userImages = [
-      {
-        id: 0,
-        title: "image1",
-        url: "image1",
-        description: "",
-        rating: 4,
-        username: "@shrek",
-        isAnimated: true,
-        code: "wsum(sin(x), cos(y))",
-        comments: "",
-      },
-      {
-        id: 1,
-        title: "image2",
-        url: "image2",
-        description: "",
-        rating: 10,
-        username: "@shrek",
-        isAnimated: false,
-        code: "cos(x)",
-        comments: "",
-      },
-      {
-        id: 2,
-        title: "image3",
-        url: "image3",
-        rating: 10,
-        username: "@shrek",
-        isAnimated: true,
-        code: "mult(cos(y),y)",
-        comments: "",
-      },
-    ];
+	// update
+	app.put("/users/:id", async (req, res) => {
+		try {
+			const { id } = req.params;
+			const { user_id, email, password } = req.body;
+			const updateUser = await pool.query(
+				"update users set (user_id, email, password)=($1, $2, $3) where user_id=$4",
+				[user_id, email, password, id]
+			);
+			res.json("User updated!");
+		} catch (error) {
+			console.error(error.message);
+		}
+	});
 
-    const userAlbums = [
-      {
-        name: "my images",
-        images: userImages,
-        createdAt: new Date().toDateString(),
-        caption: "best album ever",
-      },
-      {
-        name: "my favorites",
-        images: userImages,
-        createdAt: new Date().toDateString(),
-        caption: "my favorite images to date",
-      },
-    ];
+	// delete
+	app.delete("/users/:user_id", async (req, res) => {
+		try {
+			const { user_id } = req.params;
+			const deleteUser = await pool.query(
+				"delete from users where user_id=$1",
+				[user_id]
+			);
+			res.json("User deleted!");
+		} catch (error) {
+			console.error(error.message);
+		}
+	});
 
-    const userInfo = {
-      user: user,
-      userImages: userImages,
-      userAlbums: userAlbums,
-    };
-    res.json(userInfo);
-  });
+	// Path: /api
+	//  Dynamic content distribution - return raw data through Fetch
 
-  // provides user information needed for profile page
-  // trying to match the userSchema,
-  // but only choose the ones we are using currently for the profile
-  app.get("/api/profile", (req, res) => {
-    var user = {
-      forename: "shrek",
-      surname: "the ogre",
-      username: "theShrekster",
-      createdAt: new Date().toDateString(),
-      about: "first the worst, shrek the best",
-      profilepic: "sin(x)",
-    };
+	app.post("/api", function (req, res) {
+		api.run(req.body, req, res);
+	});
+	app.get("/api", function (req, res) {
+		api.run(req.query, req, res);
+	});
+	//------------------------------------------------
 
-    var userImages = [
-      {
-        id: 0,
-        title: "image1",
-        url: "image1",
-        description: "",
-        rating: 4,
-        username: "@shrek",
-        isAnimated: true,
-        code: "wsum(sin(x), cos(y))",
-        comments: "",
-      },
-      {
-        id: 1,
-        title: "image2",
-        url: "image2",
-        description: "",
-        rating: 10,
-        username: "@shrek",
-        isAnimated: false,
-        code: "cos(x)",
-        comments: "",
-      },
-      {
-        id: 2,
-        title: "image3",
-        url: "image3",
-        rating: 10,
-        username: "@shrek",
-        isAnimated: true,
-        code: "mult(cos(y),y)",
-        comments: "",
-      },
-    ];
+	app.get("/api/profiles/:username", (req, res) => {
+		const username = req.params.username;
+		const user = database.User.findOne({ username: username });
+		//const user = null;
+		if (user === null) {
+			user = {
+				forename: "shrek",
+				surname: "the ogre",
+				username: "theShrekster",
+				createdAt: new Date().toDateString(),
+				about: "first the worst, shrek the best",
+				profilepic: "sin(x)",
+			};
+		}
 
-    var userAlbums = [
-      {
-        name: "my images",
-        images: userImages,
-        createdAt: new Date().toDateString(),
-        caption: "best album ever",
-      },
-      {
-        name: "my favorites",
-        images: userImages,
-        createdAt: new Date().toDateString(),
-        caption: "my favorite images to date",
-      },
-    ];
+		const userImages = [
+			{
+				id: 0,
+				title: "image1",
+				url: "image1",
+				description: "",
+				rating: 4,
+				username: "@shrek",
+				isAnimated: true,
+				code: "wsum(sin(x), cos(y))",
+				comments: "",
+			},
+			{
+				id: 1,
+				title: "image2",
+				url: "image2",
+				description: "",
+				rating: 10,
+				username: "@shrek",
+				isAnimated: false,
+				code: "cos(x)",
+				comments: "",
+			},
+			{
+				id: 2,
+				title: "image3",
+				url: "image3",
+				rating: 10,
+				username: "@shrek",
+				isAnimated: true,
+				code: "mult(cos(y),y)",
+				comments: "",
+			},
+		];
 
-    var userInfo = {
-      user: user,
-      userImages: userImages,
-      userAlbums: userAlbums,
-    };
-    res.json(userInfo);
-  });
+		const userAlbums = [
+			{
+				name: "my images",
+				images: userImages,
+				createdAt: new Date().toDateString(),
+				caption: "best album ever",
+			},
+			{
+				name: "my favorites",
+				images: userImages,
+				createdAt: new Date().toDateString(),
+				caption: "my favorite images to date",
+			},
+		];
 
-  app.post('/api/emailVerification/:username', (req, res) => {
-    console.log(req.params.username);
-  });
+		const userInfo = {
+			user: user,
+			userImages: userImages,
+			userAlbums: userAlbums,
+		};
+		res.json(userInfo);
+	});
+
+	// provides user information needed for profile page
+	// trying to match the userSchema,
+	// but only choose the ones we are using currently for the profile
+	app.get("/api/profile", (req, res) => {
+		var user = {
+			forename: "shrek",
+			surname: "the ogre",
+			username: "theShrekster",
+			createdAt: new Date().toDateString(),
+			about: "first the worst, shrek the best",
+			profilepic: "sin(x)",
+		};
+
+		var userImages = [
+			{
+				id: 0,
+				title: "image1",
+				url: "image1",
+				description: "",
+				rating: 4,
+				username: "@shrek",
+				isAnimated: true,
+				code: "wsum(sin(x), cos(y))",
+				comments: "",
+			},
+			{
+				id: 1,
+				title: "image2",
+				url: "image2",
+				description: "",
+				rating: 10,
+				username: "@shrek",
+				isAnimated: false,
+				code: "cos(x)",
+				comments: "",
+			},
+			{
+				id: 2,
+				title: "image3",
+				url: "image3",
+				rating: 10,
+				username: "@shrek",
+				isAnimated: true,
+				code: "mult(cos(y),y)",
+				comments: "",
+			},
+		];
+
+		var userAlbums = [
+			{
+				name: "my images",
+				images: userImages,
+				createdAt: new Date().toDateString(),
+				caption: "best album ever",
+			},
+			{
+				name: "my favorites",
+				images: userImages,
+				createdAt: new Date().toDateString(),
+				caption: "my favorite images to date",
+			},
+		];
+
+		var userInfo = {
+			user: user,
+			userImages: userImages,
+			userAlbums: userAlbums,
+		};
+		res.json(userInfo);
+	});
+
+	app.post("/api/emailVerification/:username", (req, res) => {
+		console.log(req.params.username);
+	});
 };
-
-
