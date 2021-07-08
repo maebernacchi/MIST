8e7f93cc8c633055844c4399106011a7d1a4b844
Evelyn Gonzalez <evelyn@funnyfiesta.com>
2021-07-08 13:31:16 -0500

    drafted comment.sql

---
+++ backend/db/scripts/comment.sql
@@ -0,0 +1,12 @@
+create table comments(
+    comment_id varchar(255) not null
+    contents varchar(255) not null,
+    user_id varchar(255) not null,
+    post_id varchar(255) not null,
+    parent_comment varchar(255),
+    likes int default 0,
+
+    publish_date timestamptz not null default now()
+);
+
+--parent_comment: used for replies to other comments. if null, the comment is not a reply
