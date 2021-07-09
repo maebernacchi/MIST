656fadb5d6dcb7f2e37ecb53bc88cb5319f49064
Evelyn Gonzalez <evelyn@funnyfiesta.com>
2021-07-09 13:45:44 -0500

    drafting collections.sql

--- backend/db/scripts/collection.sql
+++ backend/db/scripts/collection.sql
@@ -7,4 +7,4 @@ create table collections(
     publish_date timestamptz not null default now()
 );
 
---parent_comment: used for replies to other comments. if null, the comment is not a reply
+--what type is contents??
