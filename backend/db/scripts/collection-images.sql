ce666675715afcf53d210393dd7b5a228a2d2838
Evelyn Gonzalez <evelyn@funnyfiesta.com>
2021-07-13 15:32:28 -0500

    redesigning collection system

---
+++ backend/db/scripts/collection-images.sql
@@ -0,0 +1,4 @@
+create table collections-images(
+    post_id varchar(255) not null foreign key,
+    collection_id varchar(255) not null foreign key
+);
--- backend/db/scripts/post.sql
+++ backend/db/scripts/post.sql
@@ -1,4 +1,5 @@
 create table posts(
+    post_id varchar(255) not null primary key,
     title varchar(255) not null,
     caption varchar(255),
     code varchar(255) not null,
