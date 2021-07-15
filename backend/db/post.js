2b046adc315158eff096adfea0cb59c5822c59d1
zaen323 <chouzaen2002@gmail.com>
2021-07-15 16:11:02 -0500

    fix collection_images reference

--- backend/db/post.js
+++ backend/db/post.js
@@ -24,114 +24,122 @@ const checkUserExists = async (column, value) => {
  * if the image exists, and false if it does not
  */
 module.exports.imageExists = async (user_id, title, callback) => {
-  if (!(await checkUserExists(user_column, user_id))) {
+	if (!(await checkUserExists(user_column, user_id))) {
 		callback("User ID does not exist!");
 	}
-    
-  return pool.query("select exists (select * from posts where (user_id=$1 and title=$2))",
-                    [user_id, title]);
+
+	return pool.query(
+		"select exists (select * from posts where (user_id=$1 and title=$2))",
+		[user_id, title]
+	);
 };
-  
 
 module.exports.collectionExists = async (user_id, title, callback) => {
-  if (!(await checkUserExists(user_column, user_id))) {
+	if (!(await checkUserExists(user_column, user_id))) {
 		callback("User ID does not exist!");
 	}
-    
-  return pool.query("select exists (select * from collections where (user_id=$1 and title=$2))",
-                    [user_id, title]);
+
+	return pool.query(
+		"select exists (select * from collections where (user_id=$1 and title=$2))",
+		[user_id, title]
+	);
 };
 
-  /**
-   * saves the new image in the database
-   * @param title title of the image
-   * @param caption caption of the image (optional)
-   * @param code code for the image
-   * @param user_id the username of the user
-   * @param visibility visibility of the image (true if public, false if private)
-   * @param callback sends the error if there is one, true
-   * if the image exists, and false if it does not
-   */
+/**
+ * saves the new image in the database
+ * @param title title of the image
+ * @param caption caption of the image (optional)
+ * @param code code for the image
+ * @param user_id the username of the user
+ * @param visibility visibility of the image (true if public, false if private)
+ * @param callback sends the error if there is one, true
+ * if the image exists, and false if it does not
+ */
 
-  //TODO: if image already is saved, override it?
-  module.exports.saveImage = async (req, callback) => {
-    if (await imageExists(req.body.user_id, req.body.title, callback)) {
-      callback("Image already exists!");
-      return;
-    }
+//TODO: if image already is saved, override it?
+module.exports.saveImage = async (req, callback) => {
+	if (await imageExists(req.body.user_id, req.body.title, callback)) {
+		callback("Image already exists!");
+		return;
+	}
 
-    pool
-        .query(
-            "insert into posts (title, caption, code, user_id, public) \
-        values ($1, $2, $3, $4, $5)", 
-            [req.body.title, req.caption, req.code, req.user_id, req.public]
-        )
-        .then((res) => {
-            callback(`Image ${req.body.title} has been created`);
-        })
-        .catch((err) => {
-            handleDBError(err, callback);
-            return;
-        });
-  };
-  
-  /**
-   * Adds an image to a collection
-   * @param {String} albumId 
-   * @param {String} imageId 
-   */
-  module.exports.addToCollection = async (req, callback) => {
-    let post_id = 0;
-    let collection_id = 0;
+	pool
+		.query(
+			"insert into posts (title, caption, code, user_id, public) \
+        values ($1, $2, $3, $4, $5)",
+			[req.body.title, req.caption, req.code, req.user_id, req.public]
+		)
+		.then((res) => {
+			callback(`Image ${req.body.title} has been created`);
+		})
+		.catch((err) => {
+			handleDBError(err, callback);
+			return;
+		});
+};
 
-    if (!(await collectionExists(req.body.user_id, req.body.collection_title, callback))) {
-      callback("Collection does not exist!");
-      return;
-    }
-    if (!(await imageExists(req.body.image_author, req.body.image_title, callback))) {
-      callback("Image does not exist!");
-      return;
-    }
+/**
+ * Adds an image to a collection
+ * @param {String} albumId
+ * @param {String} imageId
+ */
+module.exports.addToCollection = async (req, callback) => {
+	let post_id = 0;
+	let collection_id = 0;
 
-    pool
-        .query(
-            "select post_id from posts where (user_id=$1 and title=$2)",
-            [req.body.image_author, req.body.image_title]
-        )
-        .then((res) => {
-            post_id = res.rows[0].post_id;
-        })
-        .catch((err) => {
-            handleDBError(err, callback);
-            return;
-        });
+	if (
+		!(await collectionExists(
+			req.body.user_id,
+			req.body.collection_title,
+			callback
+		))
+	) {
+		callback("Collection does not exist!");
+		return;
+	}
+	if (
+		!(await imageExists(req.body.image_author, req.body.image_title, callback))
+	) {
+		callback("Image does not exist!");
+		return;
+	}
 
-    pool
-        .query(
-            "select collection_id from collections where (user_id=$1 and title=$2)",
-            [req.body.user_id, req.body.collection_title]
-        )
-        .then((res) => {
-            collection_id = res.rows[0].collection_id;
-        })
-        .catch((err) => {
-            handleDBError(err, callback);
-            return;
-        });
-            
-    pool
-        .query(
-            "insert into collection-images (post_id, collection_id) values ($1, $2)",
-            [post_id, collection_id]
-        )
-        .then((res) => {
-            callback("Image inserted into collection successfully!");
-        })
-        .catch((err) => {
-            handleDBError(err, callback);
-            return;
-        });
-  };
-  
- 
-  
+	pool
+		.query("select post_id from posts where (user_id=$1 and title=$2)", [
+			req.body.image_author,
+			req.body.image_title,
+		])
+		.then((res) => {
+			post_id = res.rows[0].post_id;
+		})
+		.catch((err) => {
+			handleDBError(err, callback);
+			return;
+		});
+
+	pool
+		.query(
+			"select collection_id from collections where (user_id=$1 and title=$2)",
+			[req.body.user_id, req.body.collection_title]
+		)
+		.then((res) => {
+			collection_id = res.rows[0].collection_id;
+		})
+		.catch((err) => {
+			handleDBError(err, callback);
+			return;
+		});
+
+	pool
+		.query(
+			"insert into collection_images (post_id, collection_id) values ($1, $2)",
+			[post_id, collection_id]
+		)
+		.then((res) => {
+			callback("Image inserted into collection successfully!");
+		})
+		.catch((err) => {
+			handleDBError(err, callback);
+			return;
+		});
+};
