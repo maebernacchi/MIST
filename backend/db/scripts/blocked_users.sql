6ab1af5353d93db46bad94e0011c9abb2ef1541e
zaen323 <chouzaen2002@gmail.com>
2021-07-15 16:14:43 -0500

    fix type

--- backend/db/scripts/blocked_users.sql
+++ backend/db/scripts/blocked_users.sql
@@ -1,4 +1,4 @@
 create table blocked_users(
-    user_id varchar(255) not null references users(user_id),
+    user_id integer not null references users(user_id),
     blocked_user_id varchar(255) not null
 );
