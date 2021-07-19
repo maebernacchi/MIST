842023af9303084cfca0e9d75ddd8715332ad2b1
zaen323 <chouzaen2002@gmail.com>
2021-07-19 16:29:27 -0500

    fucking typo

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const { verifyEmail } = require("./db/user");
 const collectionRoute = require("./api/collection");
-const populateDataRoute = require("./api/populateData")
+const populateDataRoute = require("./api/populateData");
 const galleryRoute = require("./api/gallery");
 const userRoute = require("./api/user");
 const postRoute = require("./api/post");
@@ -9,7 +9,7 @@ const workspaceRoute = require("./api/workspace");
 
 module.exports = (app) => {
 	app.use("/api/collections", collectionRoute);
-	app.use("/api/populdateData", populateDataRoute);
+	app.use("/api/populateData", populateDataRoute);
 	app.use("/api/galleries", galleryRoute);
 	app.use("/api/posts", postRoute);
 	app.use("/api/reports", reportRoute);
