35ab5e57eaf5535f341f73e8f4ab9b5967221468
zaen323 <chouzaen2002@gmail.com>
2021-07-23 11:25:16 -0500

    Add message when removing node_modules and package-lock.json

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ if [ -d "./$1/node_modules" ] || [ -f "./$1/package-lock.json" ]; then
     read -r -p "It appears that project is already set up on the $1. Would you like to re-install? [y/N]" response
     case "$response" in
         [Yy][Ee][Ss]|[Yy])
+            echo "Removing node_modules and package-lock.json..."
             (cd $1 && rm -rf node_modules package-lock.json)
             eval "${2}=true";;
         *)
