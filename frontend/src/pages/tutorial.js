a9cd858f2ddcf49f1f3815e5fd6ed900a9d425e3
Mae Bernacchi <megan.bernacchi@gmail.com>
2021-08-04 11:06:40 -0500

    fixed sign/sin typo in color

--- frontend/src/pages/tutorial.js
+++ frontend/src/pages/tutorial.js
@@ -1650,7 +1650,7 @@ const sections = [
               </Col>
               <Col>
                 4. Connect the <b>mult</b>{" "}
-                block to the red, the x to the green, and sign to the blue.
+                block to the red, the <b>x</b> to the green, and <b>sin</b> to the blue.
               </Col>
             </Row>
           </Container>
